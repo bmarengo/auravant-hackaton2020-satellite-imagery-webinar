649c560ce85eff522beabfd8e21ea385b475faec
bruno <bruno.marengo@auravant.com>
2020-11-23 18:45:36 -0300

    fix js

--- gee.js
+++ gee.js
@@ -1,57 +1,68 @@
 
-var points = ee.FeatureCollection(data_train);
-points = points.filter(ee.Filter.eq('Campania','19/20'));
-//print(points)
+var point1 = ee.Geometry.Point([-57.50209, -35.51153], 'EPSG:4326');
+var point2 = ee.Geometry.Point([-57.49819, -35.50871], 'EPSG:4326');
 
+point1 = ee.Feature(point1, {'id': 1});
+point2 = ee.Feature(point2, {'id': 2});
+
+var points = ee.FeatureCollection([point1, point2]);
+
+//points = points.filter(ee.Filter.eq('id',1));
+
+Map.addLayer(points, 'points');
+
+
+// -----------------------
+// Función de filtrado de nubes.
 function sclFilter(image){
   var scl = image.select('SCL')
     .expression("b(0) == 2 ||(b(0) > 3 && b(0) < 8)");
   return image.mask(scl);
 }
-//;
 
-// Map the function over one year of data and take the median.
-// Load Sentinel-2 TOA reflectance data.
-var dates = ['2018-12-01', '2018-12-31', 
-            '2019-01-31', '2019-02-20', 
-            //  '2019-02-15', '2019-03-15', 
-              '2019-03-15', '2019-04-15'];
 
-var dates2 = ['2019-12-20', '2020-01-15', 
-             '2020-01-31', '2020-02-20',
-             //'2020-02-25', '2020-03-20', 
-             '2020-03-20', '2020-04-01'];
+// -----------------------
+// Obtener imagen de Sentinel filtrando por fecha y geometría.
 
-var i =2;
+var start_date = '2020-04-01';
+var end_date = '2020-06-01';
 
 var im = ee.ImageCollection('COPERNICUS/S2_SR')
-    .filterBounds(geometry)
-    .filterDate(dates[i*2], dates[i*2+1])
-    // Pre-filter to get less cloudy granules.
-    .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10))
-    //.map(maskS2clouds)
-    .map(sclFilter)
-    .reduce(ee.Reducer.firstNonNull())
+    .filterBounds(points)
+    .filterDate(start_date, end_date)
+    .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 1))
+    //.map(sclFilter)
+    //.select(['B2', 'B3', 'B4'])
+    .reduce(ee.Reducer.mean());
 
-//var samples = im.sampleRegions({collection:points,projection:'EPSG:32720', scale:10});
-//print(samples);
-Map.addLayer(im, {bands: ['B4_first', 'B3_first', 'B2_first'], min: 0, max: 3000}, 'rgb');
-//print(im);
 
-var im2 = ee.ImageCollection('COPERNICUS/S2_SR')
-    .filterBounds(geometry)
-    .filterDate(dates2[i*2], dates2[i*2+1])
-    // Pre-filter to get less cloudy granules.
-    .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 30))
-    //.map(maskS2clouds)
-    .map(sclFilter)
-    .reduce(ee.Reducer.firstNonNull())
+print(im);
 
-//var samples2 = im2.sampleRegions({collection:points,projection:'EPSG:32720', scale:10})
 
-//print(samples2)
-// Display the results.
+Map.addLayer(im, {bands: ['B4_mean', 'B3_mean', 'B2_mean'], min: 0, max: 3000}, 'rgb');
 
-Map.addLayer(im2, {bands: ['B4_first', 'B3_first', 'B2_first'], min: 0, max: 3000}, 'rgb')
 
-//Map.addLayer({eeObject:points,shown:false})
+// ----------------------
+// NDVI
+
+var ndvi = ee.ImageCollection('COPERNICUS/S2_SR')
+    .filterBounds(points)
+    .filterDate(start_date, end_date)
+    .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 1))
+    //.map(sclFilter)
+    //.select(['B2', 'B3', 'B4'])
+    .first()
+    .normalizedDifference(['B8', 'B4']);
+
+print(ndvi);
+
+Map.addLayer(ndvi, {min: -1, max: 1}, 'ndvi');
+
+
+// -----------------------
+// Sampleo de Geometrías
+
+
+var samples = ndvi.sampleRegions({collection:points, projection:'EPSG:4326', scale:10});
+print(samples);
+
